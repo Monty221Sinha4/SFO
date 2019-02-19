55ec31182759fdafc7dea426a8b76b8ecfb3b04a
Monty Sinha <monty.sinha2013@live.co.uk>
2019-02-19 12:15:05 +0000

    Add files via upload

---
+++ CityTestMethod/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CityTestMethod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CityTestMethod")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6bd0934f-c459-4c0e-ab41-52d78c3e5926")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CityTestMethod/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CityTestMethod
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+
+        [TestMethod]
+        public void switch_City_Name()
+        {
+            string CityName = "Delhi";
+            switch (CityName)
+            {
+                case "Kol":
+                    CityName = "Kolkatta";
+                    break;
+                case "Mum":
+                    CityName = "Mumbai";
+                    break;
+                case  "Del":
+                    CityName = "New Delhi";
+                    break;
+            }
+            stringAssert.Equals(CityName, "New Dehli");
+        }
+    }
+}
